a2394681897791a3d01bc3b6639fe350a0221007
Arnoud Vermeer <git@iskansloos.nl>
2010-04-20 12:38:01 +0200

    Let's get going

---
+++ src/powerstats/middleware/Windows/Tools/dataread.bat
@@ -0,0 +1,6 @@
+::Windows bach file for calling the datareader.php script for taskplanner.
+::The PHP interpreter its location is derivative from the WAMP installer
+::Should Another PHP interpreter be installed
+::then do not forget to change the adres of the interpreter.
+@echo off
+C:\wamp\bin\php\php5.2.6\php.exe datareader.php
---
+++ src/powerstats/middleware/Windows/Tools/pdu.bat
@@ -0,0 +1,7 @@
+::Windows bach file for calling the pdu.php script.
+::The PHP interpreter its location is a derivative from the WAMP installer
+::Should Another PHP interpreter be installed
+::then do not forget to change the adres of the interpreter (php.exe).
+
+
+C:\wamp\bin\php\php5.2.6\php.exe pdu.php %1 %2 %3 %4 %5 %6 %7 %8 %9
