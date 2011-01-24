e4f7ede6082110e6a724ff1e0cbc0ce60b7d6131
Arnoud Vermeer <a.vermeer@freshway.biz>
2011-01-24 10:28:26 +0100

    Lol!

--- src/powerbar/settings.py
+++ src/powerbar/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'powerbars',                      # Or path to database file if using sqlite3.
         'USER': 'powerbars',                      # Not used with sqlite3.
-        'PASSWORD': 'mYRttBr',                  # Not used with sqlite3.
+        'PASSWORD': 'xxxxxxx',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
